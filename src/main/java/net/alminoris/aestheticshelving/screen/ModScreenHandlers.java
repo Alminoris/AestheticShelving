dc7a32fbe740b3f66ed3b42e61ffb453cb8a0b56
AOA <aoa@d.co>
2025-11-12 20:37:12 +0200

    #Update 2.0.0

--- src/main/java/net/alminoris/aestheticshelving/screen/ModScreenHandlers.java
+++ src/main/java/net/alminoris/aestheticshelving/screen/ModScreenHandlers.java
@@ -12,27 +12,27 @@ public class ModScreenHandlers
 {
     public static final ScreenHandlerType<ShelfScreenHandler> SHELF_SCREEN_HANDLER =
             Registry.register(Registries.SCREEN_HANDLER, Identifier.of(AestheticShelving.MOD_ID, "shelf"),
-                    new ExtendedScreenHandlerType<>(ShelfScreenHandler::new, BlockPosPayload.PACKET_CODEC));
+                    new ExtendedScreenHandlerType<>(ShelfScreenHandler::new));
 
     public static final ScreenHandlerType<StandingShelfScreenHandler> STANDING_SHELF_SCREEN_HANDLER =
             Registry.register(Registries.SCREEN_HANDLER, Identifier.of(AestheticShelving.MOD_ID, "standing_shelf"),
-                    new ExtendedScreenHandlerType<>(StandingShelfScreenHandler::new, BlockPosPayload.PACKET_CODEC));
+                    new ExtendedScreenHandlerType<>(StandingShelfScreenHandler::new));
 
     public static final ScreenHandlerType<CeilingShelfScreenHandler> CEILING_SHELF_SCREEN_HANDLER =
             Registry.register(Registries.SCREEN_HANDLER, Identifier.of(AestheticShelving.MOD_ID, "ceiling_shelf"),
-                    new ExtendedScreenHandlerType<>(CeilingShelfScreenHandler::new, BlockPosPayload.PACKET_CODEC));
+                    new ExtendedScreenHandlerType<>(CeilingShelfScreenHandler::new));
 
     public static final ScreenHandlerType<CornerShelfScreenHandler> CORNER_SHELF_SCREEN_HANDLER =
             Registry.register(Registries.SCREEN_HANDLER, Identifier.of(AestheticShelving.MOD_ID, "corner_shelf"),
-                    new ExtendedScreenHandlerType<>(CornerShelfScreenHandler::new, BlockPosPayload.PACKET_CODEC));
+                    new ExtendedScreenHandlerType<>(CornerShelfScreenHandler::new));
 
     public static final ScreenHandlerType<TowerShelfScreenHandler> TOWER_SHELF_SCREEN_HANDLER =
             Registry.register(Registries.SCREEN_HANDLER, Identifier.of(AestheticShelving.MOD_ID, "tower_shelf"),
-                    new ExtendedScreenHandlerType<>(TowerShelfScreenHandler::new, BlockPosPayload.PACKET_CODEC));
+                    new ExtendedScreenHandlerType<>(TowerShelfScreenHandler::new));
 
     public static final ScreenHandlerType<LadderShelfScreenHandler> LADDER_SHELF_SCREEN_HANDLER =
             Registry.register(Registries.SCREEN_HANDLER, Identifier.of(AestheticShelving.MOD_ID, "ladder_shelf"),
-                    new ExtendedScreenHandlerType<>(LadderShelfScreenHandler::new, BlockPosPayload.PACKET_CODEC));
+                    new ExtendedScreenHandlerType<>(LadderShelfScreenHandler::new));
 
     public static void registerScreenHandlers()
     {
